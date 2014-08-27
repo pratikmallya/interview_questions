d38d587a33e51297b390bc43d7634e508e8f78b0
pratikmallya <pratik.mallya@gmail.com>
2014-08-27 07:20:07 -0500

    ADD: jumble sort

---
+++ iterables/jumble_sort.py
@@ -0,0 +1,38 @@
+from sys import stdin
+import unittest
+
+class TestAlg(unittest.TestCase):
+    #arr = stdin.readline().strip().split()
+    def test_basics(self):
+        arr = "2 3 10 dehra dun apple wookie 78 9"
+        res = jumble_sort(arr)
+        self.assertEqual(res,
+            [2, 3, 9, 'apple', 'dehra', 'dun', 'wookie', 10, 78]
+        )
+
+def jumble_sort(arr):
+    arr = arr.strip().split()
+    int_ind = map(is_int, arr)
+    int_arr = [int(item) for item in arr if is_int(item)]
+    char_arr = [item for item in arr if not is_int(item)]
+
+    int_arr.sort(reverse=True)
+    char_arr.sort(reverse=True)
+
+    for i, item in enumerate(int_ind):
+        if item:
+            int_ind[i] = int_arr.pop()
+        else:
+            int_ind[i] = char_arr.pop()
+
+    return int_ind
+
+def is_int(item):
+    try:
+        int(item)
+        return True
+    except ValueError:
+        return False
+
+if __name__=="__main__":
+    unittest.main()
