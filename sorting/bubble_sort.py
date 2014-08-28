e78eb29a89f7f018dc790097ba705cebc51bee39
pratikmallya <pratik.mallya@gmail.com>
2014-08-28 06:09:53 -0500

    ADD: bubble sort, rename selection sort

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -13,16 +13,16 @@ import unittest
 class TestAlg(unittest.TestCase):
     def test_case_1(self):
         v = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-        insertion_sort(v)
+        bubble_sort(v)
         self.assertEqual(v, list(range(1,11)))
 
 
-def insertion_sort(v):
+def bubble_sort(v):
 
-    for i in range(len(v)):
-        min_elem = min(v[i:])
-        min_ind = v.index(min_elem)
-        v[i], v[min_ind] = v[min_ind], v[i]
+    for i in reversed(range(len(v))):
+        for j in range(i):
+            if v[j] > v[j+1]:
+                v[j], v[j+1] = v[j+1], v[j]
 
 
 if __name__ == "__main__":
---
+++ sorting/selection_sort.py
@@ -0,0 +1,29 @@
+"""
+Insertion Sort
+
+Pick lowest element, push it to the front of the list
+This will be an in-place sort. Why? Because that's a little
+more complicated.
+
+"""
+
+import unittest
+
+
+class TestAlg(unittest.TestCase):
+    def test_case_1(self):
+        v = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+        selection_sort(v)
+        self.assertEqual(v, list(range(1,11)))
+
+
+def selection_sort(v):
+
+    for i in range(len(v)):
+        min_elem = min(v[i:])
+        min_ind = v.index(min_elem)
+        v[i], v[min_ind] = v[min_ind], v[i]
+
+
+if __name__ == "__main__":
+    unittest.main()
