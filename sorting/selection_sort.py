af94eefb969c003311214ceeee6b3ee97c01f6d9
pratikmallya <pratik.mallya@gmail.com>
2014-08-28 06:24:02 -0500

    FIX: better test cases for selection sort

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -8,13 +8,20 @@ more complicated.
 """
 
 import unittest
-
+from random import sample
+from copy import deepcopy
 
 class TestAlg(unittest.TestCase):
     def test_case_1(self):
         v = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
         selection_sort(v)
         self.assertEqual(v, list(range(1,11)))
+
+        maxlen = 10000
+        v1 = sample(range(maxlen), maxlen)
+        v2 = deepcopy(v1)
+        self.assertEqual(
+            v1.sort(), selection_sort(v2))
 
 
 def selection_sort(v):
