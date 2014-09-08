d95aebcdc865d301fb4010cd9a2c3cfc02282c3b
pratikmallya <pratik.mallya@gmail.com>
2014-09-08 02:24:57 -0500

    ADD: removed flake8 errors

--- graphs/find_subtree.py
+++ graphs/find_subtree.py
@@ -5,8 +5,7 @@ a depth-first representation, the question will be one of string matching.
 
 """
 import sys
-from operator import concat
-from functools import reduce
+from collections import deque
 
 
 def main():
@@ -20,56 +19,57 @@ def main():
         tree1 = build_tree(t1)
         tree2 = build_tree(t2)
 
-    print(tree1)
-    print(tree2)
-
     ans = is_subtree(tree1, tree2)
     sys.stdout.write("{}\n".format(ans))
 
+
 def build_tree(line):
     """build a tree from the string description"""
-    line = line.split(',')
-    heads = [line.pop(0)]
+    line = deque(line.split(','))
+    heads = deque([line.popleft()])
     tree = {}
-    node_index = [0]
+    node_index = deque([0])
 
     while line:
 
-        head = heads.pop(0)
+        head = heads.popleft()
 
         if head != '':
-            lchild, rchild, node_index, head_add = find_children(line, node_index)
+            lchild, rchild, node_index, head_add = find_children(line,
+                                                                 node_index)
 
-            tree[node_index.pop(0)] = {'lchild': lchild,
-                                'rchild': rchild,
-                                 'value': int(head),
-                                }
+            tree[node_index.popleft()] = {'lchild': lchild,
+                                          'rchild': rchild,
+                                          'value': int(head),
+                                          }
             heads += head_add
         else:
-            heads += [line.pop(0), line.pop(0)]
+            heads += deque([line.popleft(), line.popleft()])
 
     for head in heads:
         if head != '':
-            tree[node_index.pop(0)] =  {'lchild': None,
-                           'rchild': None,
-                           'value': int(head),
-                           }
+            tree[node_index.popleft()] = {'lchild': None,
+                                          'rchild': None,
+                                          'value': int(head),
+                                          }
 
     return tree
 
+
 def find_children(line, node_index):
-    head_add = []
+    """find the children to the present node and assign proper indices"""
+    head_add = deque([])
     lchild = None
     rchild = None
 
-    head = line.pop(0)
+    head = line.popleft()
     if head:
         node_index.append(node_index[-1] + 1)
         lchild = node_index[-1]
 
     head_add.append(head)
 
-    head = line.pop(0)
+    head = line.popleft()
     if head:
         node_index.append(node_index[-1] + 1)
         rchild = node_index[-1]
@@ -80,7 +80,7 @@ def find_children(line, node_index):
 
 
 def is_subtree(t1, t2):
-
+    """find if t1 is a subtree of t2"""
     # find node in t2 with same value as root(t1)
     common_nodes = get_common_nodes(t1, t2)
     # for each such node, check if the trees are equivalent
@@ -89,8 +89,10 @@ def is_subtree(t1, t2):
             return True
     return False
 
+
 def get_common_nodes(t1, t2):
-    """find node in t2 with same value as root(t1)"""
+    """find nodes in t2 with same value as root(t1)"""
+
     root_value = t1[0]['value']
     for node in t2.keys():
         if t2[node]['value'] == root_value:
@@ -98,19 +100,22 @@ def get_common_nodes(t1, t2):
 
 
 def tree_diff(node1, node2, t1, t2):
+    """find if t1 is a subtree of t2 with the help of root nodes
+    node1 and node2"""
 
-    # if both are None, return True
-    if node1 == node2 == None:
+    if node1 == node2 is None:  # leaf node
         return True
-    # if either one is None, return False
-    if node1 == None or node2 == None:
-        print("node1:{} node2:{}".format(node1, node2))
+    elif node1 is None:  # reached end of t1
+        return True
+    elif node2 is None:  # reached end of t2
         return False
 
     if t1[node1]['value'] == t2[node2]['value']:
         return (tree_diff(t1[node1]['lchild'], t2[node2]['lchild'], t1, t2) and
                 tree_diff(t1[node1]['rchild'], t2[node2]['rchild'], t1, t2)
                 )
+    else:
+        return False
 
 
 if __name__ == "__main__":
