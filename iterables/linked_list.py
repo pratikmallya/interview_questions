c692c68945d8cce3d98a493e3c26e36f9899806c
pratikmallya <pratik.mallya@gmail.com>
2014-09-14 15:59:19 -0500

    FIX: linked list now works, passes tests

--- iterables/linked_list.py
+++ iterables/linked_list.py
@@ -8,13 +8,24 @@ import unittest
 class TestLinkedList(unittest.TestCase):
 
     def test_1(self):
+
         N = 10
-        ll = LinkedList(N)
-        self.assertEqual(ll.data, N)
+
         ll = LinkedList(range(N))
-        for index in range(N):
-            self.assertEqual(ll.data, index)
-            ll = ll.next
+
+        for index, item in enumerate(ll):
+            self.assertEqual(item, index)
+
+        self.assertFalse(ll.find(-15))
+
+        self.assertTrue(ll.insert(-15))
+        self.assertTrue(ll.find(-15))
+
+        self.assertTrue(ll.delete(-15))
+        self.assertFalse(ll.find(-15))
+
+        self.assertTrue(ll.delete(1))
+        self.assertFalse(ll.find(1))
 
 
 class LinkedList(object):
@@ -25,20 +36,74 @@ class LinkedList(object):
     """
 
     def __init__(self, data):
+        """Initializes a Linked List
+
+        My implementation of a linked list uses a dummy first element
+        Arguments:
+            data: either a single object or iterable
+        """
+        self.data = None
+        self.next = None
+
         if isinstance(data, Iterable):
             ll = self.__build_list(data)
             self.data = ll.data
             self.next = ll.next
         else:
             self.data = data
-            self.next = None
 
-    def __build_list(self, data):
-        head = first = LinkedList(None)
-        for node in data:
-            head.next = LinkedList(node)
-            head = head.next
-        return first.next
+    def __build_list(self, datas):
+        """build the list from the iterable
+
+        Returns:
+            reference to the head of created list
+        """
+        head = node = LinkedList(None)
+        for data in datas:
+            node.next = LinkedList(data)
+            node = node.next
+        return head.next
+
+    def insert(self, data):
+        """insert element data in the linked list
+        """
+        node = self
+        while node.next:
+            node = node.next
+        node.next = LinkedList(data)
+        return True
+
+    def delete(self, data):
+        """delete element from linked list"""
+        prev = self
+        node = self.next
+        while node:
+            if node.data == data:
+                prev.next = node.next
+                del node
+                return True
+            prev = node
+            node = node.next
+        return False
+
+    def find(self, data):
+        """check if element exists in linked list"""
+        node = self.next
+
+        while node:
+            if node.data == data:
+                return True
+            node = node.next
+
+        return False
+
+    def __iter__(self):
+        """yield data values stored in linked list"""
+        node = self
+
+        while node:
+            yield node.data
+            node = node.next
 
 
 if __name__ == "__main__":
